7cdba9e8d0043c6f43cefc7cdf9378b0b3495f35
Lincoln <contato.joydamasceno@gmail.com>
2022-12-09 20:26:29 -0300

    first commit

--- android/app/src/main/kotlin/com/example/exemplo_state/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.exemplo_state
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/app/modules/novo/novo_module_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:exemplo_state/app/modules/novo/novo_module.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:modular_test/modular_test.dart';
-
-void main() {
-  setUpAll(() {
-    initModule(NovoModule());
-  });
-}
